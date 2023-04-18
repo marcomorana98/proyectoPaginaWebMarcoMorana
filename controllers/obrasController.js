397e28bbdb663eb8e094a669aab00c4580737a93
marcomorana98 <marcomorana982@gmail.com>
2023-04-18 12:50:55 -0300

    Obras actualiza la entrada y salida del obrero

--- controllers/obrasController.js
+++ controllers/obrasController.js
@@ -1,27 +1,86 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-const mysql = require('mysql2');
-const path = require('path');
-app.set('views', path.join(__dirname, 'views'));
+const mysql = require("mysql2");
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+var { format } = require("date-fns");
 
 const controller = {
-    mostrarObras: function (req, res) {
-        connection.query('SELECT * FROM obras', function(error, arrayObras, fields){
-          res.render('obras', {foo: arrayObras});
-    })
-      },
+  mostrarObras: function (req, res) {
+    connection.query(
+      "SELECT * FROM obras",
+      function (error, arrayObras, fields) {
+        connection.query(
+          "SELECT idUsuarios FROM usuarios WHERE username = ?",
+          [req.session.username],
+          function (error, idObrero, fields) {
+            let date = new Date();
+            let fechaHoy = format(date, "yyyy-MM-dd");
+            connection.query(
+              "SELECT * FROM asistencias WHERE fecha = ? AND idObrero = ?",
+              [fechaHoy, idObrero[0].idUsuarios],
+              function (error, arrayAsistencias, fields) {
+                console.log(arrayAsistencias);
+                let asistenciasActivas = arrayAsistencias.find(
+                  (o) => o.idobra != 0
+                );
+                console.log(asistenciasActivas);
+                if (arrayAsistencias.horaSalida != 0) {
+                  res.render("obras", {
+                    obras: arrayObras,
+                    obrero: idObrero[0].idUsuarios,
+                    asistido: arrayAsistencias,
+                  });
+                } else {
+                  res.render("obras", {
+                    obras: arrayObras,
+                    obrero: idObrero[0].idUsuarios,
+                    asistido: 0,
+                  });
+                }
+              }
+            );
+          }
+        );
+      }
+    );
+  },
 
-    asistirAObra: function (req, res) {
-        let obra = req.body.obra;
-        let fecha = req.body.fecha;
-        let hora = req.body.hora
-        let obrero = req.session.username
-        let nuevo = req.body.nuevo
-        console.log(nuevo)
-        connection.query('INSERT INTO asistencias VALUES (?, ?, ?, ?, ?)', [obra, obrero, fecha, hora, 0], function(error, results, fields) {
-          res.redirect('/obras');
-    })
-    }
-}
+  asistirAObra: function (req, res) {
+    let bandera = 0;
+    let { obra, obrero, nuevo } = req.body;
+    let date = new Date();
+    let fecha = format(date, "yyyy-MM-dd");
+    let hora = format(date, "HH:mm:ss");
+    connection.query(
+      "INSERT INTO asistencias VALUES (?, ?, ?, ?, ?)",
+      [obra, obrero, fecha, hora, 0],
+      function (error, results, fields) {
+        if (error) {
+          connection.query(
+            "SELECT idUsuarios FROM usuarios WHERE username = ?",
+            [req.session.username],
+            function (error, idObrero, fields) {
+              let date = new Date();
+              let fechaHoy = format(date, "yyyy-MM-dd");
+              let hora = format(date, "HH:mm:ss");
+              connection.query(
+                "UPDATE asistencias SET horaSalida = ? WHERE fecha = ? AND idObrero = ? AND idObra = ?",
+                [hora, fechaHoy, idObrero[0].idUsuarios, parseInt(obra)],
+                function (error, arrayObras, fields) {
+                  if (error) {
+                    console.log(error);
+                  }
+                }
+              );
+            }
+          );
+          console.log("tiene que dar salida antes de dar otra entrada");
+        }
+        res.redirect("/obras");
+      }
+    );
+  },
+};
 
 module.exports = controller;
